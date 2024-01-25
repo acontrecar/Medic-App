c26ade906f75b045b799c2c713356d122043947c
Antonio Contreras <acontrecar@gmail.com>
2024-01-25 17:03:47 +0100

    initial creations

---
+++ src/app/shared/components/navbar-login/navbar-login.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'shared-navbar-login',
+  standalone: true,
+  imports: [],
+  templateUrl: './navbar-login.component.html',
+  styleUrl: './navbar-login.component.scss'
+})
+export class NavbarLoginComponent {
+
+}
---
+++ src/app/shared/components/navbar-main/navbar-main.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'shared-navbar-main',
+  standalone: true,
+  imports: [],
+  templateUrl: './navbar-main.component.html',
+  styleUrl: './navbar-main.component.scss'
+})
+export class NavbarMainComponent {
+
+}
