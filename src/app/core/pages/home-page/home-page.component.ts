62d8030515de2c15527341f98238e6194e8dc0f8
Antonio Contreras <acontrecar@gmail.com>
2024-01-30 15:46:46 +0100

    deploy

--- src/app/core/pages/home-page/home-page.component.ts
+++ src/app/core/pages/home-page/home-page.component.ts
@@ -5,8 +5,8 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './home-page.component.html',
-  styleUrl: './home-page.component.scss'
+  styleUrl: './home-page.component.scss',
 })
 export class HomePageComponent {
-
+  public pacientes: String[] = ['Jose', 'Paco', 'Roberta'];
 }
