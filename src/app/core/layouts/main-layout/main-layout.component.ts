f41429e213d0be57225b73642f579f69e667978e
Antonio Contreras <acontrecar@gmail.com>
2024-01-30 10:15:18 +0100

    first commit Develop

--- src/app/core/layouts/main-layout/main-layout.component.ts
+++ src/app/core/layouts/main-layout/main-layout.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarMainComponent } from '../../../shared/components/navbar-main/navbar-main.component';
+import { FooterComponent } from '../../../shared/components/footer/footer.component';
 
 @Component({
   selector: 'app-main-layout',
   standalone: true,
-  imports: [RouterOutlet, NavbarMainComponent],
+  imports: [RouterOutlet, NavbarMainComponent, FooterComponent],
   templateUrl: './main-layout.component.html',
-  styleUrl: './main-layout.component.scss'
+  styleUrl: './main-layout.component.scss',
 })
-export class MainLayoutComponent {
-
-}
+export class MainLayoutComponent {}
